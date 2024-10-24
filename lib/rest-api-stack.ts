cdab7af6b1f3caaf377d6211da05caff811fb026
Dawid Beres <20098868@mail.wit.ie>
2024-10-24 19:07:11 +0200

    Exercise: Delete movie endpoint

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -35,6 +35,18 @@ export class RestAPIStack extends cdk.Stack {
       },
     });
 
+    const deleteMovieFn = new lambdanode.NodejsFunction(this, "DeleteMovieFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      entry: `${__dirname}/../lambdas/deleteMovie.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        TABLE_NAME: moviesTable.tableName,
+        REGION: "eu-west-1",
+      },
+    });
+
     const getMovieByIdFn = new lambdanode.NodejsFunction(
       this,
       "GetMovieByIdFn",
@@ -87,6 +99,7 @@ export class RestAPIStack extends cdk.Stack {
         moviesTable.grantReadData(getMovieByIdFn)
         moviesTable.grantReadData(getAllMoviesFn)
         moviesTable.grantReadWriteData(newMovieFn)
+        moviesTable.grantReadWriteData(deleteMovieFn)
 
         // REST API 
     const api = new apig.RestApi(this, "RestAPI", {
@@ -115,6 +128,7 @@ export class RestAPIStack extends cdk.Stack {
     );
 
     moviesEndpoint.addMethod("POST", new apig.LambdaIntegration(newMovieFn, { proxy: true}));
+    moviesEndpoint.addMethod("DELETE", new apig.LambdaIntegration(deleteMovieFn, { proxy: true}));
         
       }
     }
